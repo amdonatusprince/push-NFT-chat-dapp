0e6218bca54466844df4e98d469f3bcb5cb5dbb0
= <=>
2023-07-29 05:55:58 +0100

    feat: push socket setup

--- live-chat-app/client/src/context/ChatProvider.js
+++ live-chat-app/client/src/context/ChatProvider.js
@@ -1,7 +1,9 @@
-import React, { createContext, useContext, useState } from 'react';
-import io from 'socket.io-client';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { createSocketConnection, EVENTS } from '@pushprotocol/socket';
 
-const socket = io.connect(process.env.REACT_APP_SERVER || window.location.host);
+const user = '0xD8634C39BBFd4033c0d3289C4515275102423681';
+const chainId = 5;
+const userCAIP = `eip155:${chainId}:${user}`;
 
 const ChatContext = createContext();
 
@@ -12,9 +14,40 @@ export const useChat = () => {
 export const ChatProvider = ({ children }) => {
     const [userName, setUserName] = useState('');
     const [currentRoom, setCurrentRoom] = useState(null);
+    const [sdkSocket, setSDKSocket] = useState(null);
     
+    useEffect(() => {
+        const connectionObject = createSocketConnection({
+            user: userCAIP,
+            env: 'dev',
+            socketOptions: { autoConnect: true } // Start the connection automatically
+        });
+
+        setSDKSocket(connectionObject);
+
+        return () => {
+            if (sdkSocket) {
+                sdkSocket.disconnect();
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        if (sdkSocket) {
+            sdkSocket.on(EVENTS.CONNECT, () => {
+                console.log('Connected to the Push Protocol server');
+            });
+
+            sdkSocket.on(EVENTS.DISCONNECT, () => {
+                console.log('Disconnected from the Push Protocol server');
+            });
+
+            // You can add more event listeners here if needed.
+        }
+    }, [sdkSocket]);
+
     const value = {
-        socket,
+        sdkSocket,
         userName,
         setUserName,
         setCurrentRoom,
@@ -22,8 +55,9 @@ export const ChatProvider = ({ children }) => {
     };
     
     return (
-        <ChatContext.Provider value={ value }>
-            { children }
+        <ChatContext.Provider value={value}>
+            {children}
         </ChatContext.Provider>
+        
     );
 };
